cbfceaeb58dc83adec213138f86996ea4605b161
Kayla Ward <kaylaw0127@gmail.com>
2020-03-02 22:37:00 -0800

    added username prompt

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+inquirer
+  .prompt({
+    message: "Enter your GitHub username:",
+    name: "username"
+  })
